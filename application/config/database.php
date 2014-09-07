294ffcb82debd73dc94580c32138ec45ec6917d8
gremdev <grem.ociones@gmail.com>
2014-09-08 00:00:51 +0800

    Restore Default Project settings and files

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'cyberspace@14';
+$db['default']['database'] = 'basecamp_final';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
